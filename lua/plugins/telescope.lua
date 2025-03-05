fb44935ea3369b6acea675b633586be37ccc80bc
ManasC478 <manas.chougule@gmail.com>
2025-03-04 22:54:23 -0800

    fix: change live grep mapping

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,7 +5,7 @@ return {
     config = function()
       local builtin = require("telescope.builtin")
       vim.keymap.set('n', 'ff', builtin.find_files, {})
-      vim.keymap.set('n', 'fg', builtin.live_grep, {})
+      vim.keymap.set('n', 'lg', builtin.live_grep, {})
     end
   },
   {
